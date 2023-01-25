c1517ab5dd6887afb04b092642c994542ce266ec
Aghil P Wilson <aghilpwilson1994@gmail.com>
2023-01-25 16:37:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Form from './Form';
+import TodoList from './TodoList';
 
 function App() {
+  const localInput = JSON.parse(localStorage.getItem("todos")) || [];
+  const [input,setInput] = useState("");
+  const [todos,setTodos] = useState(localInput);
+  const [editTodo,setEditTodo] = useState(null);
+
+  useEffect(()=>{
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="app_wrapper">
+      <div className="header">
+        <h1>Todo List</h1>
+    </div>
+    <Form 
+      input = {input}
+      setInput = {setInput}
+      todos = {todos}
+      setTodos = {setTodos}
+      editTodo = {editTodo}
+      setEditTodo = {setEditTodo}
+    />
+     <div>
+        <TodoList todos = {todos} setTodos = {setTodos} setEditTodo = {setEditTodo} />
+      </div>
+      </div>
+     
     </div>
   );
 }
